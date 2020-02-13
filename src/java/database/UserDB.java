7ebae15da5049e187142c1558a4015141ec63170
0809379 <0809379@KELCE-LABS13-P.pittstate.edu>
2020-02-13 12:11:57 -0600

    class1

---
+++ src/java/database/UserDB.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package database;
+
+import businessBean.User;
+
+/**
+ *
+ * @author 0809379
+ */
+public class UserDB {
+    
+    public static long insertUser(User user){
+        long testing =1234;
+        
+        return testing;
+    }
+}
---
+++ src/java/servlets/EmailListServlet.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package servlets;
+
+
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+import businessBean.User;
+import database.UserDB;
+
+public class EmailListServlet extends HttpServlet {
+        
+       protected void doPost(HttpServletRequest request, 
+                          HttpServletResponse response) 
+                          throws ServletException, IOException {
+
+        String url = "/index.html";
+
+        // get current action
+        String action = request.getParameter("action");
+        if (action == null) {
+            action = "join";  // default action
+        }
+
+        // perform action and set URL to appropriate page
+        if (action.equals("join")) {
+            url = "/index.html";    // the "join" page
+        }
+        else if (action.equals("add")) {                
+            // get parameters from the request
+            String firstName = request.getParameter("firstName");
+            String lastName = request.getParameter("lastName");
+            String email = request.getParameter("email");
+
+            // store data in User object and save User object in database
+            User user = new User(firstName, lastName, email);
+            User.insert(user);
+            
+            // set User object in request object and set URL
+            request.setAttribute("user", user);
+            url = "/thanks.jsp";   // the "thanks" page
+        }
+        
+        // forward request and response objects to specified URL
+        getServletContext()
+            .getRequestDispatcher(url)
+            .forward(request, response);
+    }    
+    
+    protected void doGet(HttpServletRequest request, 
+                          HttpServletResponse response) 
+                          throws ServletException, IOException {
+        doPost(request, response);
+    }    
+}
